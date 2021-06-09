05abc6e43615373ce7eddb4f81d9b9ae77d2abcc
dfleis <david.p.fleischer@gmail.com>
2021-06-09 08:19:09 -0400

    cleaning up prototyping tests

--- tests/test-cv/biglasso-cv-test.R
+++ tests/test-cv/biglasso-cv-test.R
@@ -3,10 +3,10 @@ library(glmnet)
 library(biglasso)
 library(survival)
 
+################### GENERATE DATA 
 set.seed(124)
-### GENERATE DATA 
-no <- 100
-nx <- 25
+no <- 2500
+nx <- 1000
 dat <- coxed::sim.survdata(N = no, xvars = nx)
 X <- as.matrix(dat$xdata)
 y <- cbind("time" = as.numeric(dat$data$y), "status" = dat$data$failed)
@@ -16,17 +16,20 @@ Xbig <- as.big.matrix(X)
 penalty  <- "enet"
 alpha    <- 0.5 # elastic net penalty (alpha = 0 is ridge and alpha = 1 is lasso)
 nfolds   <- 5
-lambda   <- exp(seq(-1, -7, length.out = 100))
+lambda   <- exp(seq(0, -6, length.out = 100))
 grouped  <- T # grouped = F is 'basic' CV loss, grouped = T is 'V&VH' CV loss. see https://arxiv.org/pdf/1905.10432.pdf
-parallel <- F # "use parallel  to fit each fold. Must register parallel before hand, such as doMC or others"
+parallel <- T # "use parallel  to fit each fold. Must register parallel before hand, such as doMC or others"
+ncores   <- 5
 trace.it <- 1
 
+################### RUN CV MODELS
 
 # lapply(list.files("../cv-biglasso-cox/R/", full.names = T), source)
 # 
-# 
-# ### RUN CROSS-VALIDATED MODELS
 # pt <- proc.time()
+# NOTE: This one has to be run first if we want to keep the cv.ind/foldid the same 
+# across the other cv functions (I didn't code in a way to input cv fold indices
+# in this earlier version)
 # cv.blR <- cv.biglasso.cox(x        = Xbig,
 #                           y        = y,
 #                           penalty  = penalty,
@@ -46,12 +49,12 @@ cv.bl <- cv.biglasso(X       = Xbig,
                      nfolds  = nfolds,
                      lambda  = lambda,
                      grouped = grouped,
-                     ncores  = 1,
-                     trace   = as.logical(trace.it),
-                     cv.ind  = cv.blR$foldid)
+                     ncores  = ncores,
+                     trace   = as.logical(trace.it))
 proc.time() - pt
 
 
+if (parallel) {doMC::registerDoMC(cores = ncores)}
 
 pt <- proc.time()
 cv.gn <- cv.glmnet(x        = X,                               
@@ -67,8 +70,15 @@ cv.gn <- cv.glmnet(x        = X,
 proc.time() - pt
 
 
-#plot.cv.biglasso.cox(cv.blR)
-#plot.compare.cv(cv.blR, cv.gn)
+
+
+#############################
 
 plot(cv.bl)
 plot(cv.gn)
+
+
+plot(cv.bl$cve ~ lambda, log = 'x', type = 'l', lwd = 2)
+lines(cv.gn$cvm ~ lambda, col = 'red', lty = 'dashed', lwd = 2)
+
+#plot(cv.gn$cvm - cv.bl$cve ~ lambda, log = 'x', type = 'l', lwd = 2)
